d2cbcf9463c7df7ec18b8cb901a896dd5e74c9dd
Saad E <21321555+se447@users.noreply.github.com>
2019-03-21 20:17:44 -0400

    Defined java class for exponent program

---
+++ src/SumOfThePowers.java
@@ -0,0 +1,11 @@
+//Create a program that calculates the sum of 2^0+2^1+2^2+...+2^n, where n is a number entered by the user.
+import java.util.Scanner;
+
+public class SumOfThePowers {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        
+    }
+}

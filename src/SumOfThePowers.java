be89ca24625906403d2e1d603bf04cc7a65abad9
Saad E <21321555+se447@users.noreply.github.com>
2019-03-21 20:22:45 -0400

    Calculated sum of exponents of 2

--- src/SumOfThePowers.java
+++ src/SumOfThePowers.java
@@ -6,6 +6,16 @@ public class SumOfThePowers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type a number: ");
+        int exponent = Integer.parseInt(reader.nextLine());
         
+        int sum = 0;
+        int number = 0;
+        while (number <= exponent) {
+            sum += (int)Math.pow(2, number);
+            number++;
+        }
+        
+        System.out.println("The result is " + sum);
     }
 }
